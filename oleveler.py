b7a0df5f02cde61e0bd0a300f35f7e75ddf1ebdc
Dr. C. Du <c.du@biology.leidenuniv.nl>
2021-10-28 11:59:30 +0200

    add overwrite option to writeRSessionInfo()

--- oleveler.py
+++ oleveler.py
@@ -62,7 +62,7 @@ if len(logger.handlers) == 0:
     logger.addHandler(logshandler)
     logger.setLevel(logging.DEBUG)
 
-def writeRSessionInfo(fileName, logger=None):
+def writeRSessionInfo(fileName, overwrite=True, logger=None):
     
     def printToString(*args, **kwargs):
         with io.StringIO() as output:
@@ -72,16 +72,20 @@ def writeRSessionInfo(fileName, logger=None):
 
     if isinstance(logger, type(None)):
         logger = logging.getLogger()
+    sinfo = 'Attached packages in current R session:\n' + '=' * 80 + '\n'
+    for l in robjects.r('sessionInfo()["otherPkgs"]')[0]:
+        x = printToString(l).split('\n')
+        y = [a for a in x if len(a)>0 and not a.startswith('-- File:')]
+        z = '\n'.join(y) + '\n\n' + "=" * 80 + '\n'
+        sinfo += z
+    logger.info(sinfo)
+    if not overwrite:
+        if os.path.isfile(fileName):
+            logger.info(f'File {fileName} exists, will not overwrite.')
+            return 0
     with open(fileName, 'w') as fh:
-        fh.write('Attached packages in current R session:\n' + '=' * 80 + '\n')
-        sinfo = ''
-        for l in robjects.r('sessionInfo()["otherPkgs"]')[0]:
-            x = printToString(l).split('\n')
-            y = [a for a in x if len(a)>0 and not a.startswith('-- File:')]
-            z = '\n'.join(y) + '\n\n' + "=" * 80 + '\n'
-            sinfo += z
         fh.write(sinfo)
-        logger.info(sinfo)
+    return 1
 
 
 # ==================== LOAD DATA =======================
