ab37dcc74540e65c43ee640c5994957754d3bfaf
Dr. C. Du <c.du@biology.leidenuniv.nl>
2021-10-28 11:41:25 +0200

    Add function to get r session info

--- oleveler.py
+++ oleveler.py
@@ -12,6 +12,7 @@ import pickle
 import logging
 import sys
 import json
+import io
 from multiprocessing import Process
 from threading import Thread
 from tempfile import NamedTemporaryFile
@@ -60,6 +61,28 @@ if len(logger.handlers) == 0:
     logger.addHandler(logfhandler)
     logger.addHandler(logshandler)
     logger.setLevel(logging.DEBUG)
+
+def writeRSessionInfo(fileName, logger=None):
+    
+    def printToString(*args, **kwargs):
+        with io.StringIO() as output:
+            print(*args, file=output, **kwargs)
+            contents = output.getvalue()
+        return contents
+
+    if isinstance(logger, type(None)):
+        logger = logging.getLogger()
+    with open(fileName, 'w') as fh:
+        fh.write('Attached packages in current R session:\n' + '=' * 80 + '\n')
+        sinfo = ''
+        for l in robjects.r('sessionInfo()["otherPkgs"]')[0]:
+            x = printToString(l).split('\n')
+            y = [a for a in x if len(a)>0 and not a.startswith('-- File:')]
+            z = '\n'.join(y) + '\n\n' + "=" * 80 + '\n'
+            sinfo += z
+        fh.write(sinfo)
+        logger.info(sinfo)
+
 
 # ==================== LOAD DATA =======================
 
