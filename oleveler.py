d4666853e65ae6885782004d727cd40796d62314
Dr. C. Du <c.du@biology.leidenuniv.nl>
2021-11-03 11:05:15 +0100

    optimise displayimage with custom background of different format (hard code html)

--- oleveler.py
+++ oleveler.py
@@ -86,12 +86,29 @@ if len(logger.handlers) == 0:
 logger.info(infoText)
 
 
-def displayImage(src, background=None):
+def displayImage(src, background=None, **kwargs):
+    from IPython.display import Image, SVG, display
     from IPython.core.display import HTML
-    htmlstr = f'<img src={src} />'
-    if not isinstance(background, type(None)):
-        htmlstr = htmlstr[:-2] + f'style="background-color:{background}"' + htmlstr[-2:]
-    return HTML(htmlstr)
+    if src[-4:].lower() == '.svg':
+        htmlstr = '<div class="jp-RenderedSVG jp-OutputArea-output " data-mime-type="image/svg+xml">'
+        svgstr = SVG(data=src)._repr_svg_()
+        if not isinstance(background, type(None)):
+            bgstr = f'style="background-color:{background}"'
+            svgstr = '<svg ' + bgstr + svgstr[4:]
+        htmlstr += svgstr + '</div>'
+        display(HTML(htmlstr))
+    else:
+        img = Image(data=src)
+        fmt = img.format
+        if fmt == 'png':
+            htmlstr = '<div class="jp-RenderedImage jp-OutputArea-output ">'
+            htmlstr += f'\n<img src="data:image/png;base64,{img._repr_png_()}"></div>'
+            if not isinstance(background, type(None)):
+                htmlstr = htmlstr[:-7] + f'style="background-color:{background}"' + htmlstr[-7:]
+            display(HTML(htmlstr))
+        else:
+            logger.warning('background for none PNG image will be ignored')
+            display(img)
 
 
 def setDarkModePlotting(forceDark=False):
