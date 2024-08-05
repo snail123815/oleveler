97160d6a3a942c17fb493d61b86e6d707a2e2aa4
Chao Du <snail123815@users.noreply.github.com>
2024-08-05 23:21:50 +0200

    add logger import

--- oleveler/__init__.py
+++ oleveler/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import pickle
+from pathlib import Path
 import rpy2.robjects as robjects # run this here or in .main can supress INFO: "R is already initialized. No need to initialize."
 
 from .main import *
@@ -13,3 +16,4 @@ from .MaxQuant_data_processor import *
 from .RNASeq_processor import *
 from .metadata_processor import loadMeta, removeExperiments, removeIds
 from .basic_stats import getStats
+from .project_logger import logfhandler, logshandler
