0844cf0d7b4139119772171c940d22dee0103112
Chao Du <duc@vblis.iblbioinfo.lu.nl>
2024-10-07 20:28:11 +0200

    add compression_processor.py imports in __init__.py

--- oleveler/__init__.py
+++ oleveler/__init__.py
@@ -1,19 +1,19 @@
 import logging
 import pickle
 from pathlib import Path
-import rpy2.robjects as robjects # run this here or in .main can supress INFO: "R is already initialized. No need to initialize."
-
-from .main import *
-
-from .side_notebook_functions import \
-    displayImage, \
-    setDarkModePlotting, \
-    writeRSessionInfo
-
 from time import sleep
 
-from .MaxQuant_data_processor import *
-from .RNASeq_processor import *
-from .metadata_processor import loadMeta, removeExperiments, removeIds
+import rpy2.robjects as robjects  # run this here or in .main can supress INFO: "R is already initialized. No need to initialize."
+
 from .basic_stats import getStats
+from .comparison_processor import genComparisonResults, getSig
+from .main import *
+from .MaxQuant_data_processor import *
+from .metadata_processor import loadMeta, removeExperiments, removeIds
 from .project_logger import logfhandler, logshandler
+from .RNASeq_processor import *
+from .side_notebook_functions import (
+    displayImage,
+    setDarkModePlotting,
+    writeRSessionInfo,
+)
